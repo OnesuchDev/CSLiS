1faea885bb224606d3c8f8c9df32b5711cfc4bef
OnesuchDev <onesuchdev@gmail.com>
2026-08-19 19:36:45 -0700

    Set -Wno-address-of-packed-member if the compiler supports it
    This disables an unfixed warning in <linux/objpool.h>, seen on kernel 7.2.

--- config.mk
+++ config.mk
@@ -145,9 +145,9 @@ XOPTS+= -D"KBUILD_BASENAME=KBUILD_STR(streams)"
 # Compiler options for debugging and optimization.
 
 ifeq ($(DBG_OPT),y)
-XOPTS += -ggdb -O $(CC_OPT2) $(CC_OPT3) $(CC_OPT4)
+XOPTS += -ggdb -O $(CC_OPT2) $(CC_OPT3) $(CC_OPT4) $(CC_OPT5)
 else
-XOPTS += $(CC_OPTIMIZE) $(CC_OPT2) $(CC_OPT3) $(CC_OPT4) -DINLINE=inline -DSTATIC=static -std=gnu11 -fomit-frame-pointer
+XOPTS += $(CC_OPTIMIZE) $(CC_OPT2) $(CC_OPT3) $(CC_OPT4) $(CC_OPT5) -DINLINE=inline -DSTATIC=static -std=gnu11 -fomit-frame-pointer
 endif
 
 # Add -fcf-protection if it is required
