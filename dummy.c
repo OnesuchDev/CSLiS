6c51cd6fe0ab1ba36aba812dce76904531ecb716
OnesuchDev <onesuchdev@gmail.com>
2026-08-22 20:48:55 -0700

    Get rid of the dummy.c template
    The comments are unnecessary, so the entire file can be generated at runtime.

--- dummy.c
+++
@@ -1,6 +0,0 @@
-/* #
-   # Copyright 2022 - IBM Inc. All rights reserved
-   # SPDX-License-Identifier: LGPL-2.1
-# */
-/* dummy file to allow use of cc to untangle autoconf.h/version.h */
-#include <generated/utsrelease.h>
